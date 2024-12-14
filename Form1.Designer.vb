1c5c08d672b79490e1961c0badcf047879912fb3
Sara <Sara@DESKTOP-R2BTG91>
2024-12-14 11:57:59 +0000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(57, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(161, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Escolha um Idioma"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Items.AddRange(New Object() {"Português", "Inglês", "Espanhol", "Italiano", "Alemão", "Francês"})
+        Me.ListBox1.Location = New System.Drawing.Point(101, 104)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(70, 100)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(98, 244)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(16, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "..."
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(101, 297)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(144, 123)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(98, 272)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(16, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "..."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(931, 546)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Reflection.Emit
+
+Public Class Form1
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+        Select Case ListBox1.SelectedItem.ToString()
+            Case "Português"
+                Label2.Text = "Olá!!! Bem-vindos a Portugal!"
+                PictureBox1.Image = Image.FromFile("D:\Os meus documentos\IEFP - EFA Programador Informática\3934 - Visual Basic .NET\Visual Basic\Ex2Select\Portugal.png")
+                Label3.Text = "Capital: Lisboa"
+
+            Case "Inglês"
+                Label2.Text = "Hello!!! Welcome to England!"
+                PictureBox1.Image = Image.FromFile("D:\Os meus documentos\IEFP - EFA Programador Informática\3934 - Visual Basic .NET\Visual Basic\Ex2Select\Inglaterra.png")
+                Label3.Text = "Capital: London"
+
+            Case "Espanhol"
+                Label2.Text = "Hola!!! Bienvenidos a España!"
+                PictureBox1.Image = Image.FromFile("D:\Os meus documentos\IEFP - EFA Programador Informática\3934 - Visual Basic .NET\Visual Basic\Ex2Select\Espanha.png")
+                Label3.Text = "Capital: Madrid"
+
+            Case "Francês"
+                Label2.Text = "Bonjour! Bienvenue en France!"
+                PictureBox1.Image = Image.FromFile("D:\Os meus documentos\IEFP - EFA Programador Informática\3934 - Visual Basic .NET\Visual Basic\Ex2Select\França.png")
+                Label3.Text = "Capital: Paris"
+
+            Case "Alemão"
+                Label2.Text = "Hallo! Willkommen in Deutschland!"
+                PictureBox1.Image = Image.FromFile("D:\Os meus documentos\IEFP - EFA Programador Informática\3934 - Visual Basic .NET\Visual Basic\Ex2Select\Alemanha.png")
+                Label3.Text = "Capital: Berlim"
+
+            Case "Italiano"
+                Label2.Text = "EHI! Benvenuti in Italia!"
+                PictureBox1.Image = Image.FromFile("D:\Os meus documentos\IEFP - EFA Programador Informática\3934 - Visual Basic .NET\Visual Basic\Ex2Select\Italia.png")
+                Label3.Text = "Capital: Roma"
+
+        End Select
+    End Sub
+
+End Class
+
